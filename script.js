dbb5ad5567ac55e12d9259217e5f134590cad268
Mohamed <mohamed77massawi@gmail.com>
2022-12-28 14:29:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
+
+var swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  // loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  //   pauseOnMouseEnter: true,
+  // },
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+const openButton = document.getElementById("open-button");
+const closeButton = document.getElementById("close-button");
+const menu = document.getElementById("menu");
+
+function toggleMenu() {
+  if (menu.classList.contains("active-menu")) {
+    menu.classList.remove("active-menu");
+  } else {
+    menu.classList.add("active-menu");
+  }
+}
+
+openButton.addEventListener("click", toggleMenu);
+closeButton.addEventListener("click", toggleMenu);
